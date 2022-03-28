afa3d8b83e6dafa4cd52daba4a9ca5c265c8404f
SafiaMaani <saf.maani@gmail.com>
2022-03-28 13:52:46 +0100

    conditions des résultats

--- js/algo.js
+++ js/algo.js
@@ -1,6 +1,9 @@
-let facteursGravitéMineurs = 0;
-let facteursGravitéMajeurs = 0;
-let facteurPronostique = 0;
+//FACTEURS DE GRAVITES
+let facteursGravité = {
+    facteursGravitéMineurs: 0,
+    facteursGravitéMajeurs: 0
+}
+
 let fievre = false;
 let toux = false;
 let courbatures = false;
@@ -11,17 +14,18 @@ let difficulteAvalaison = false;
 let manqueSouffle = false;
 let sensation = '';
 //FACTEURS PRONOSTIQUES
-let age;
-let hypertension = false;
-let diabete = false;
-let cancer = false;
-let maladieRespiratoire = false;
-let dialyse = false;
-let foie = false;
-let enceiente = false;
-let diminutionImmunitaire = false;
-let traitementImmunosuppresseur = false;
-
+let facteurPronostique = {
+    age,
+    hypertension: false,
+    diabete: false,
+    cancer: false,
+    maladieRespiratoire: false,
+    dialyse: false,
+    foie: false,
+    enceiente: false,
+    diminutionImmunitaire: false,
+    traitementImmunosuppresseur: false
+} = 0
 
 
 //qst1 : FIEVRE
@@ -31,10 +35,10 @@ if (reponse == 'oui') {
 
 //qst2 : TEMPERATURE
 if (temperature >= 39) {
-    facteursGravitéMineurs++;
+    facteursGravité.facteursGravitéMineurs++;
 }
 if (temperature <= 35.4) {
-    facteursGravitéMajeurs++;
+    facteursGravité.facteursGravitéMajeurs++;
 }
 
 //qst3 : TOUX
@@ -60,24 +64,24 @@ if (reponse == 'oui') {
 //qst7 :  FATIGUE
 if (reponse == 'oui') {
     fatigue = true;
-    facteursGravitéMineurs++;
+    facteursGravité.facteursGravitéMineurs++;
 }
 
 //qst8 : DIFFICULTES D'AVALAISON
 if (reponse == 'oui') {
     difficulteAvalaison = true;
-    facteursGravitéMajeurs++;
+    facteursGravité.facteursGravitéMajeurs++;
 }
 
 //qst9 : MANQUE DE SOUFFLE
 if (reponse == 'oui') {
     manqueSouffle = true;
-    facteursGravitéMajeurs++;
+    facteursGravité.facteursGravitéMajeurs++;
 }
 
 //qst10 : SENSATION
 if (sensation == 'fatigué' || sensation == 'très fatigué') {
-    facteursGravitéMineurs++;
+    facteursGravité.facteursGravitéMineurs++;
 }
 
 //qst11 : AGE (facteur de risque spécifique)
@@ -86,63 +90,63 @@ if (sensation == 'fatigué' || sensation == 'très fatigué') {
 
 //qst14 : l’hypertension, maladie cardiaque ou vasculaire
 if (reponse == 'oui') {
-    hypertension = true;
+    facteurPronostique.hypertension = true;
 }
 
 //qst15 : DIABETE
 if (reponse == 'oui') {
-    diabete = true;
+    facteurPronostique.diabete = true;
 }
 
 //qst16 : CANCER
 if (reponse == 'oui') {
-    cancer = true
+    facteurPronostique.cancer = true
 }
 
 //qst17 : Maladie respiratoire
 if (reponse == 'oui') {
-    maladieRespiratoire = true;
+    facteurPronostique.maladieRespiratoire = true;
 }
 
 //qst18 : Insuffisance rénale chronique dialysée
 if (reponse == 'oui') {
-    dialyse = true;
+    facteurPronostique.dialyse = true;
 }
 
 //qst19 : MALADIE DU FOIE
 if (reponse == 'oui') {
-    foie = true
+    facteurPronostique.foie = true
 }
 
 //qst20 : ENCEINTE
 if (reponse == 'oui') {
-    enceiente = true;
+    facteurPronostique.enceiente = true;
 }
 
 //qst21 : une maladie connue pour diminuer vos défenses immunitaires
 if (reponse == 'oui') {
-    diminutionImmunitaire = true;
+    facteurPronostique.diminutionImmunitaire = true;
 }
 
 //qst22 : traitement immunosuppresseur
 if (reponse == 'oui') {
-    traitementImmunosuppresseur = true;
+    facteurPronostique.traitementImmunosuppresseur = true;
 }
 
 //RESULTS
 if ((fievre == true || toux == true) && (malGorge == true || toux == true) && (courbatures == true || fievre == true) && (diarrhee == true)) {
     if (facteurPronostique == 0) {
-        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0 && age < 50) {
+        if (facteursGravité == 0 && age < 50) {
             console.log(' nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’ apparition de nouveaux symptômes.Vous pourrez aussi utiliser à nouveau l’ application pour réévaluer vos symptômes.')
         }
-        if ((facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0 && age > 50 && age < 69) || facteursGravitéMineurs >= 1) {
+        if ((facteursGravité == 0 && age > 50 && age < 69) || facteursGravitéMineurs >= 1) {
             console.log('téléconsultation ou médecin généraliste ou visite à domicile')
             if (manqueSouffle == true || difficulteAvalaison == true) {
                 console.log('appelez le 141 ')
             }
         }
     } else if (facteurPronostique >= 1) {
-        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
+        if (facteursGravité == 0) {
             console.log(': téléconsultation ou médecin généraliste ou visite à domicile')
             if (manqueSouffle == true || difficulteAvalaison == true) {
                 console.log('appelez le 141 ')
@@ -161,26 +165,31 @@ if ((fievre == true || toux == true) && (malGorge == true || toux == true) && (c
 
 } else if (fievre == true && toux == true) {
     if (facteurPronostique == 0) {
-        if ((facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) || (facteursGravitéMineurs >= 1 && facteursGravitéMajeurs == 0)) {
+        if ((facteursGravité == 0) || (facteursGravitéMineurs >= 1 && facteursGravitéMajeurs == 0)) {
             console.log(' téléconsultation ou médecin généraliste ou visite à domicile')
         }
     } else if (facteurPronostique >= 1) {
-        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
+        if (facteursGravité == 0) {
             console.log('téléconsultation ou médecin généraliste ou visite à domicile ')
         } else if (facteursGravitéMineurs >= 1) {
             console.log('téléconsultation ou médecin généraliste ou visite à domicile ')
         } else if (facteursGravitéMineurs >= 2) {
             console.log('Appel 141')
         }
+    } else if (facteursGravitéMajeurs >= 1) {
+        console.log('Appel 141')
     }
 } else if (fievre == true || toux == true || malGorge == true || courbatures == true) {
-
-} else {
-
+    if (facteursGravité == 0) {
+        console.log('Votre situation ne relève probablement pas du Covid-19. Consultez votre médecin au moindre doute.')
+    } else if (facteurPronostique >= 1 || facteursGravitéMajeurs >= 1 || facteursGravitéMineurs >= 1) {
+        console.log('Votre situation ne relève probablement pas du Covid-19. Un avis médical est recommandé.Au moindre doute, appelez le 141. ')
+    }
+} else if (facteurPronostique == 0 && facteursGravité == 0) {
+    console.log('Votre situation ne relève probablement pas du Covid-19. N’hésitez pas à contacter votre médecin en cas de doute.Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation.Pour toute information concernant le Covid - 19 allez vers la page d’ accueil.')
 }
 //Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
 //Tout patient avec fièvre et toux :
 //Tout patient avec un seul symptôme parmi fièvre, toux, mal de gorge, courbatures :
 //Tout patient avec aucun symptôme :
-
 //Pour tous :
