5b4286b69ed24c9d61140289e40208cc90627692
SafiaMaani <saf.maani@gmail.com>
2022-03-29 09:39:04 +0100

    safia's commits

--- js/script.js
+++ js/script.js
@@ -253,11 +253,10 @@ function checkNum() {
     quiz[cont].reponse = num.value
 }
 
-//FACTEURS DE GRAVITES (si un element dans l'objet est incrementé l'objet aussi sera incrementé et si les 2 egal 0 l'objet aussi = 0)
-let facteursGravité = {
-    facteursGravitéMineurs: 0,
-    facteursGravitéMajeurs: 0
-}
+//FACTEURS DE GRAVITES 
+let facteursGravitéMineurs = 0
+let facteursGravitéMajeurs = 0
+
 
 //SYMPTOMES
 let symptômes = {
@@ -293,15 +292,10 @@ if (quiz[0].reponse == 'oui') {
 
 //qst2 : TEMPERATURE
 if (temperature >= 39) {
-    facteursGravité.facteursGravitéMineurs++;
+    facteursGravitéMineurs++;
 }
 if (temperature <= 35.4) {
-    facteursGravité.facteursGravitéMajeurs++;
-}
-//qst1 : FIEVRE33
-if (quiz[cont].reponse == 'oui') {
-    fievre == true;
-
+    facteursGravitéMajeurs++;
 }
 
 //qst3 : TOUX
@@ -328,60 +322,27 @@ if (quiz[5].reponse == 'oui') {
 //qst7 :  FATIGUE
 if (quiz[6].reponse == 'oui') {
     symptômes.fatigue = true;
-    facteursGravité.facteursGravitéMineurs++;
+    facteursGravitéMineurs++;
 }
 
 //qst8 : DIFFICULTES D'AVALAISON
 if (quiz[7].reponse == 'oui') {
     symptômes.difficulteAvalaison = true;
-    facteursGravité.facteursGravitéMajeurs++;
+    facteursGravitéMajeurs++;
 }
 
 //qst9 : MANQUE DE SOUFFLE
 if (quiz[8].reponse == 'oui') {
     symptômes.manqueSouffle = true;
-    facteursGravité.facteursGravitéMajeurs++;
+    facteursGravitéMajeurs++;
     if (quiz[cont].reponse == 'oui') {
         toux = true;
     }
 }
 
-//qst4 : COURBATURES 
-if (quiz[cont].reponse == 'oui') {
-    courbatures = true;
-}
-
-//qst5 : MAL DE GORGE
-if (quiz[cont].reponse == 'oui') {
-    malGorge = true;
-}
-
-//qst6 : DIARREE
-if (quiz[cont].reponse == 'oui') {
-    diarrhee = true
-}
-
-//qst7 :  FATIGUE
-if (quiz[cont].reponse == 'oui') {
-    fatigue = true;
-    facteursGravitéMineurs++;
-}
-
-//qst8 : DIFFICULTES D'AVALAISON
-if (quiz[cont].reponse == 'oui') {
-    difficulteAvalaison = true;
-    facteursGravitéMajeurs++;
-}
-
-//qst9 : MANQUE DE SOUFFLE
-if (quiz[cont].reponse == 'oui') {
-    manqueSouffle = true;
-    facteursGravitéMajeurs++;
-}
-
 //qst10 : SENSATION
 if (sensation == 'fatigué' || sensation == 'très fatigué') {
-    facteursGravité.facteursGravitéMineurs++;
+    facteursGravitéMineurs++;
 }
 
 //qst11 : AGE (facteur de risque spécifique)
@@ -389,7 +350,6 @@ if (sensation == 'fatigué' || sensation == 'très fatigué') {
 //qst13 : TAILLE
 
 //qst14 : l’hypertension, maladie cardiaque ou vasculaire
-
 if (quiz[13].reponse == 'oui') {
     facteursPronostiques.hypertension = true;
 }
@@ -437,19 +397,18 @@ if (quiz[21].reponse == 'oui') {
 //RESULTS
 if ((symptômes.fievre == true || symptômes.toux == true) && (symptômes.malGorge == true || symptômes.toux == true) && (symptômes.courbatures == true || symptômes.fievre == true) && (symptômes.diarrhee == true)) {
     if (facteursPronostiques == 0) {
-        if (facteursGravité == 0 && age < 50) {
+        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0 && age < 50) {
             quiz.push(' nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’ apparition de nouveaux symptômes.Vous pourrez aussi utiliser à nouveau l’ application pour réévaluer vos symptômes.')
         }
 
-        if ((facteursGravité == 0 && age > 50 && age < 69) || facteursGravitéMineurs >= 1) {
+        if ((facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0 && age > 50 && age < 69) || facteursGravitéMineurs >= 1) {
             quiz.push('téléconsultation ou médecin généraliste ou visite à domicile')
             if (manqueSouffle == true || difficulteAvalaison == true) {
                 quiz.push('appelez le 141 ')
             }
         }
-    } else
-    if (facteursPronostiques >= 1) {
-        if (facteursGravité == 0) {
+    } else if (facteursPronostiques >= 1) {
+        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
             quiz.push('téléconsultation ou médecin généraliste ou visite à domicile')
             if (manqueSouffle == true || difficulteAvalaison == true) {
                 quiz.push('appelez le 141 ')
@@ -466,14 +425,13 @@ if ((symptômes.fievre == true || symptômes.toux == true) && (symptômes.malGor
         quiz.push('Appel 141')
     }
 
-} else
-if (symptômes.fievre == true && symptômes.toux == true) {
+} else if (symptômes.fievre == true && symptômes.toux == true) {
     if (facteursPronostiques == 0) {
-        if ((facteursGravité == 0) || (facteursGravitéMineurs >= 1 && facteursGravitéMajeurs == 0)) {
+        if ((facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) || (facteursGravitéMineurs >= 1 && facteursGravitéMajeurs == 0)) {
             quiz.push(' téléconsultation ou médecin généraliste ou visite à domicile')
         }
     } else if (facteursPronostiques >= 1) {
-        if (facteursGravité == 0) {
+        if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
             quiz.push('téléconsultation ou médecin généraliste ou visite à domicile ')
         } else if (facteursGravitéMineurs >= 1) {
             quiz.push('téléconsultation ou médecin généraliste ou visite à domicile ')
@@ -484,54 +442,11 @@ if (symptômes.fievre == true && symptômes.toux == true) {
         quiz.push('Appel 141')
     }
 } else if (symptômes.fievre == true || symptômes.toux == true || symptômes.malGorge == true || symptômes.courbatures == true) {
-    if (facteursGravité == 0) {
+    if (facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
         quiz.push('Votre situation ne relève probablement pas du Covid-19. Consultez votre médecin au moindre doute.')
     } else if (facteursPronostiques >= 1 || facteursGravitéMajeurs >= 1 || facteursGravitéMineurs >= 1) {
         quiz.push('Votre situation ne relève probablement pas du Covid-19. Un avis médical est recommandé.Au moindre doute, appelez le 141. ')
     }
-} else if (facteursPronostiques == 0 && facteursGravité == 0) {
+} else if (facteursPronostiques == 0 && facteursGravitéMajeurs == 0 && facteursGravitéMineurs == 0) {
     quiz.push('Votre situation ne relève probablement pas du Covid-19. N’hésitez pas à contacter votre médecin en cas de doute.Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation.Pour toute information concernant le Covid - 19 allez vers la page d’ accueil.')
-}
-if (quiz[cont].reponse == 'oui') {
-    hypertension = true;
-}
-
-//qst15 : DIABETE
-if (quiz[cont].reponse == 'oui') {
-    diabete = true;
-}
-
-//qst16 : CANCER
-if (quiz[cont].reponse == 'oui') {
-    cancer = true
-}
-
-//qst17 : Maladie respiratoire
-if (quiz[cont].reponse == 'oui') {
-    maladieRespiratoire = true;
-}
-
-//qst18 : Insuffisance rénale chronique dialysée
-if (quiz[cont].reponse == 'oui') {
-    dialyse = true;
-}
-
-//qst19 : MALADIE DU FOIE
-if (quiz[cont].reponse == 'oui') {
-    foie = true
-}
-
-//qst20 : ENCEINTE
-if (quiz[cont].reponse == 'oui') {
-    enceiente = true;
-}
-
-//qst21 : une maladie connue pour diminuer vos défenses immunitaires
-if (quiz[cont].reponse == 'oui') {
-    diminutionImmunitaire = true;
-}
-
-//qst22 : traitement immunosuppresseur
-if (quiz[cont].reponse == 'oui') {
-    traitementImmunosuppresseur = true;
 }
